6787c72bc821e63e8e468e1725c2a1e3fe7256e3
unknown <16tsmith55@gmail.com>
2020-02-17 01:14:37 -0700

    animations and finish line

--- main.js
+++ main.js
@@ -1,36 +1,55 @@
-let runway = document.getElementById("runway");
 let index = 0;
+let runway = document.getElementById("runway");
 
-$.getJSON(
-  "https://v2-api.sheety.co/483642879b0fbfc0b773ec3bd0a70c0c/racecarBulletin/sheet1",
-  function(roster_raw) {
-    let roster = roster_raw["sheet1"];
+async function run() {
+  let res = await fetch(
+    "https://v2-api.sheety.co/483642879b0fbfc0b773ec3bd0a70c0c/racecarBulletin/sheet1"
+  )
+    .then(res => res.json())
+    .then(data => {
+      let roster = data["sheet1"];
 
-    roster.sort(function(a, b) {
-      return b.score - a.score;
-    });
+      roster.sort(function(a, b) {
+        return b.score - a.score;
+      });
 
-    Object.keys(roster).forEach(function(student) {
-      let track = document.createElement("div");
-      track.setAttribute("class", "track");
+      Object.keys(roster).forEach(function(student) {
+        let track = document.createElement("div");
+        let car = document.createElement("i");
+        let label = document.createElement("p");
+        let score = document.createElement("span");
 
-      let label = document.createElement("p");
-      label.innerHTML = roster[student].name;
-      label.setAttribute("class", "label");
-      track.appendChild(label);
+        track.setAttribute("class", "track");
+        score.setAttribute("class", "score");
 
-      let progress = document.createElement("input");
-      progress.setAttribute("class", "slider");
-      progress.setAttribute("value", roster[student].score);
-      progress.setAttribute("type", "range");
-      progress.setAttribute("min", 0);
-      progress.setAttribute("max", 30);
+        label.innerHTML = roster[student].name;
+        score.innerHTML = roster[student].score;
 
-      track.appendChild(progress);
+        label.setAttribute("class", "label");
+        car.setAttribute("class", "car fas fa-car-side");
 
-      runway.appendChild(track);
+        track.appendChild(label);
+        track.appendChild(car);
+        label.appendChild(score);
+        runway.appendChild(track);
 
-      index++;
-    });
-  }
-);
+        let pos =
+          (car.parentNode.offsetWidth * roster[student].score) /
+            roster[student].max -
+          20 -
+          label.offsetWidth -
+          car.offsetWidth;
+
+        if (roster[student].score == 1) {
+          car.style.transform = `translateX(6px)`;
+        } else if (roster[student].score == 2) {
+          car.style.transform = `translateX(16px)`;
+        } else {
+          car.style.transform = `translateX(${pos}px)`;
+        }
+      });
+    })
+    .then(() => {});
+}
+
+run();
