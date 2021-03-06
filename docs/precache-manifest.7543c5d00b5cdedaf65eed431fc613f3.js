b5a6137f37001ea379d87812c5c0934f796941fe
trevsm <trevsm@pm.me>
2021-03-05 21:40:18 -0700

    renamed build to docs

---
+++ docs/precache-manifest.7543c5d00b5cdedaf65eed431fc613f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "961d21a156f4ed06064addfb8381f572",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3821e03b2ccd4ee6390",
+    "url": "/static/css/2.7d0b1304.chunk.css"
+  },
+  {
+    "revision": "b172099502e9601c6499",
+    "url": "/static/css/main.54df9ba1.chunk.css"
+  },
+  {
+    "revision": "d3821e03b2ccd4ee6390",
+    "url": "/static/js/2.3d8a0131.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.3d8a0131.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b172099502e9601c6499",
+    "url": "/static/js/main.6d871aca.chunk.js"
+  },
+  {
+    "revision": "da6844d8a89e5a2f44b0",
+    "url": "/static/js/runtime-main.ac4b94d8.js"
+  }
+]);
